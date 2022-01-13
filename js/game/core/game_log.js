ed8c26cfc52ca1dfe4d9a87ed6a8caceecf5b394
Juan Manuel Flecha <flechajm@gmail.com>
2022-01-12 23:25:49 -0300

    Mejoras en el manejo del Log.
    - Cambio de nombre de clase.
    - Agregado de auto-scroll.
    - Agregado de método de guardado de progreso.

--- js/game/core/game_log.js
+++ js/game/core/game_log.js
@@ -1,12 +1,26 @@
-class Log {
+class GameLog {
   static logDOM = $("#log > div.text");
 
+  static #append(html) {
+    this.logDOM.append(html);
+    this.logDOM.append("<br />");
+    this.logDOM.animate(
+      {
+        scrollTop: this.logDOM.offset().top,
+      },
+      2000
+    );
+  }
+
   static write(html, color) {
-    this.logDOM.append(`<span style="color: ${color ?? "inherit"};">${html}</span>`);
-    this.newLine();
+    this.#append(`<span style="color: ${color ?? "inherit"};">${html}</span>`);
   }
 
   static newLine(cant) {
-    this.logDOM.append("<br />".repeat(cant ?? 1));
+    this.#append("<br />".repeat(cant ?? 1));
+  }
+
+  static writeSaveProgress() {
+    this.write(`${LanguageManager.getData().saveProgress} [${new Date().toLocaleString()}]`, "grey");
   }
 }
