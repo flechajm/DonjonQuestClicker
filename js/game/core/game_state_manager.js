c6317761d9579e643314d7c63a276a6e365e3432
Juan Manuel Flecha <flechajm@gmail.com>
2022-01-13 20:24:01 -0300

    Update game_state_manager.js

--- js/game/core/game_state_manager.js
+++ js/game/core/game_state_manager.js
@@ -4,13 +4,13 @@
 class GameStateManager {
   static save() {
     gameManager.config.saveDate = new Date().getTime();
-    localStorage.setItem(GameInfo.storageName, JSON.stringify(gameManager));
+    localStorage.setItem(GameInfo.storageName, JSON.stringify(gameManager).encrypt());
   }
 
   static load() {
     let gameManager = localStorage.getItem(GameInfo.storageName);
     if (gameManager) {
-      return new GameManager(JSON.parse(localStorage.getItem(GameInfo.storageName)));
+      return new GameManager(JSON.parse(localStorage.getItem(GameInfo.storageName).decrypt()));
     }
   }
 }
